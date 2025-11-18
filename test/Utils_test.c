09de6b7af37fa4a2798950b14299f54e21cfb8a8
manuel <magana272@berkeley.edu>
2025-11-18 03:31:06 -0800

    utils tests

--- test/Utils_test.c
+++ test/Utils_test.c
@@ -5,10 +5,24 @@
 int validate_args_test(int argc, char *argv[])
 {
 
-    return 0;
+    int result = validate_args(argc, argv);
+    if (result == -1)
+    {
+        printf("Argument validation failed.\n");
+    }
+    else
+    {
+        printf("Argument validation succeeded.\n");
+    }
+    return result;
 }
 void getFileAndScheduleType_test(char **file_input, ScheduleType *schedule_type, int argc, char *argv[])
 {
+    int verbose_mode = 0;
+    getFileAndScheduleType(file_input, schedule_type, &verbose_mode, argc, argv);
+    printf("File Input: %s\n", *file_input);
+    printf("Schedule Type: %d\n", *schedule_type);
+    printf("Verbose Mode: %d\n", verbose_mode);
 }
 
 int main()
@@ -16,7 +30,7 @@ int main()
     char *file_input = "test.txt";
     ScheduleType schedule_type;
     int argc = 2;
-    char *argv[] = {"./test", "arg1"};
+    char *argv[] = {"./test", "RR"};
 
     getFileAndScheduleType_test(&file_input, &schedule_type, argc, argv);
 
